8792987068c3b32ca1e8d2a657115c1f886ba05b
AndrewZastovnik <Andrew.Zastovnik@gmail.com>
2017-05-04 12:55:31 -0700

    Moved the RKM function outside

--- R/clustering.R
+++ R/clustering.R
@@ -54,159 +54,6 @@ clustering <- function(Weights, method, k, t=NULL, sparse=T,
         
         
         if (kmeans.method=="RKM") {
-            
-            ###
-            ### Define the RKM function
-            ###
-            
-            RKM <- function (data, nclus, ndim, alpha = NULL, method = "RKM", center = TRUE, 
-                scale = TRUE, rotation = "none", nstart = 10, smartStart = NULL, 
-                seed = 1234) {
-                
-                require(ggplot2)
-                require(dummies)
-                require(grid)
-                require(corpcor)
-                
-                ssq = function(a) {
-                    t(as.vector(c(as.matrix(a))))%*%as.vector(c(as.matrix(a)))
-                }
-                
-                if (is.null(alpha) == TRUE) {
-                    if (method == "RKM") {
-                        alpha = 0.5
-                    }
-                    else if (method == "FKM") {
-                        alpha = 0
-                    }
-                }
-                odata = data
-                data = scale(data, center = center, scale = scale)
-                # data = data.matrix(data)
-                n = dim(data)[1]
-                m = dim(data)[2]
-                conv = 1e-06
-                func = {
-                }
-                index = {
-                }
-                AA = {
-                }
-                FF = {
-                }
-                YY = {
-                }
-                UU = {
-                }
-                
-                require(irlba)
-                
-                for (run in c(1:nstart)) {
-                    if (is.null(smartStart)) {
-                        myseed = seed + run
-                        set.seed(myseed)
-                        randVec = matrix(ceiling(runif(n) * nclus), n, 1)
-                    }
-                    else {
-                        randVec = smartStart
-                    }
-                    U = dummy(randVec)
-                    P = U %*% pseudoinverse(t(U) %*% U) %*% t(U)
-                    
-                    
-                    # A = eigen(t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * 
-                    #         diag(n)) %*% data)$vectors
-                    # A = A[, 1:ndim]
-                    
-                    
-                    testobj <- t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * diag(n)) %*% data
-                    
-                    A <- partial_eigen(x=testobj, n = ndim, symmetric = TRUE)$vectors
-                    
-                    
-                    G = data %*% A
-                    Y = pseudoinverse(t(U) %*% U) %*% t(U) %*% G
-                    f = alpha * ssq(data - G %*% t(A)) + (1 - alpha) * ssq(data %*% 
-                            A - U %*% Y)
-                    f = as.numeric(f)
-                    fold = f + 2 * conv * f
-                    iter = 0
-                    while (f < fold - conv * f) {
-                        fold = f
-                        iter = iter + 1
-                        outK = try(kmeans(G, centers = Y, nstart = 100), 
-                            silent = T)
-                        if (is.list(outK) == FALSE) {
-                            outK = EmptyKmeans(G, centers = Y)
-                        }
-                        v = as.factor(outK$cluster)
-                        U = diag(nlevels(v))[v, ]
-                        P = U %*% pseudoinverse(t(U) %*% U) %*% t(U)
-                        
-                        # A = eigen(t(data) %*% ((1 - alpha) * P - (1 - 2 * 
-                        #         alpha) * diag(n)) %*% data)$vectors
-                        # A = A[, c(1:ndim)]
-                        
-                        testobj <- t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * diag(n)) %*% data
-                        
-                        A <- partial_eigen(x=testobj, n = ndim, symmetric = TRUE)$vectors
-                        
-                        
-                        G = data %*% A
-                        Y = pseudoinverse(t(U) %*% U) %*% t(U) %*% G
-                        f = alpha * ssq(data - G %*% t(A)) + (1 - alpha) * 
-                            ssq(data %*% A - U %*% Y)
-                    }
-                    func[run] = f
-                    FF[[run]] = G
-                    AA[[run]] = A
-                    YY[[run]] = Y
-                    UU[[run]] = U
-                    cat("Just finished iteration ", run, "\n")
-                }
-                mi = which.min(func)
-                U = UU[[mi]]
-                cluID = apply(U, 1, which.max)
-                csize = round((table(cluID)/sum(table(cluID))) * 100, digits = 2)
-                aa = sort(csize, decreasing = TRUE)
-                require(plyr)
-                cluID = mapvalues(cluID, from = as.integer(names(aa)), to = as.integer(names(table(cluID))))
-                centroid = YY[[mi]]
-                centroid = centroid[as.integer(names(aa)), ]
-                if (rotation == "varimax") {
-                    require(stats)
-                    AA[[mi]] = varimax(AA[[mi]])$loadings
-                    FF[[mi]] = data %*% AA[[mi]]
-                    centroid = pseudoinverse(t(U) %*% U) %*% t(U) %*% FF[[mi]]
-                    centroid = centroid[as.integer(names(aa)), ]
-                }
-                else if (rotation == "promax") {
-                    AA[[mi]] = promax(AA[[mi]])$loadings[1:m, 1:ndim]
-                    FF[[mi]] = data %*% AA[[mi]]
-                    centroid = pseudoinverse(t(U) %*% U) %*% t(U) %*% FF[[mi]]
-                    centroid = centroid[as.integer(names(aa)), ]
-                }
-                out = list()
-                mi = which.min(func)
-                out$obscoord = FF[[mi]]
-                rownames(out$obscoord) = rownames(data)
-                out$attcoord = data.matrix(AA[[mi]])
-                rownames(out$attcoord) = colnames(data)
-                out$centroid = centroid
-                names(cluID) = rownames(data)
-                out$cluID = cluID
-                out$criterion = func[mi]
-                out$csize = round((table(cluID)/sum(table(cluID))) * 100, 
-                    digits = 1)
-                out$odata = odata
-                out$scale = scale
-                out$center = center
-                out$nstart = nstart
-                class(out) = "cluspca"
-                return(out)
-            }
-            
-            
             ###
             ### Run RKM on the normalized Z matrix
             ###
@@ -342,159 +189,6 @@ clustering <- function(Weights, method, k, t=NULL, sparse=T,
         # why the average part?
         
         if (kmeans.method=="RKM") {
-            
-            ###
-            ### Define the RKM function
-            ###
-            
-            RKM <- function (data, nclus, ndim, alpha = NULL, method = "RKM", center = TRUE, 
-                scale = TRUE, rotation = "none", nstart = 10, smartStart = NULL, 
-                seed = 1234) {
-                
-                require(ggplot2)
-                require(dummies)
-                require(grid)
-                require(corpcor)
-                
-                ssq = function(a) {
-                    t(as.vector(c(as.matrix(a))))%*%as.vector(c(as.matrix(a)))
-                }
-                
-                if (is.null(alpha) == TRUE) {
-                    if (method == "RKM") {
-                        alpha = 0.5
-                    }
-                    else if (method == "FKM") {
-                        alpha = 0
-                    }
-                }
-                odata = data
-                data = scale(data, center = center, scale = scale)
-                # data = data.matrix(data)
-                n = dim(data)[1]
-                m = dim(data)[2]
-                conv = 1e-06
-                func = {
-                }
-                index = {
-                }
-                AA = {
-                }
-                FF = {
-                }
-                YY = {
-                }
-                UU = {
-                }
-                
-                require(irlba)
-                
-                for (run in c(1:nstart)) {
-                    if (is.null(smartStart)) {
-                        myseed = seed + run
-                        set.seed(myseed)
-                        randVec = matrix(ceiling(runif(n) * nclus), n, 1)
-                    }
-                    else {
-                        randVec = smartStart
-                    }
-                    U = dummy(randVec)
-                    P = U %*% pseudoinverse(t(U) %*% U) %*% t(U)
-                    
-                    
-                    # A = eigen(t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * 
-                    #         diag(n)) %*% data)$vectors
-                    # A = A[, 1:ndim]
-                    
-                    
-                    testobj <- t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * diag(n)) %*% data
-                    
-                    A <- partial_eigen(x=testobj, n = ndim, symmetric = TRUE)$vectors
-                    
-                    
-                    G = data %*% A
-                    Y = pseudoinverse(t(U) %*% U) %*% t(U) %*% G
-                    f = alpha * ssq(data - G %*% t(A)) + (1 - alpha) * ssq(data %*% 
-                            A - U %*% Y)
-                    f = as.numeric(f)
-                    fold = f + 2 * conv * f
-                    iter = 0
-                    while (f < fold - conv * f) {
-                        fold = f
-                        iter = iter + 1
-                        outK = try(kmeans(G, centers = Y, nstart = 100), 
-                            silent = T)
-                        if (is.list(outK) == FALSE) {
-                            outK = EmptyKmeans(G, centers = Y)
-                        }
-                        v = as.factor(outK$cluster)
-                        U = diag(nlevels(v))[v, ]
-                        P = U %*% pseudoinverse(t(U) %*% U) %*% t(U)
-                        
-                        # A = eigen(t(data) %*% ((1 - alpha) * P - (1 - 2 * 
-                        #         alpha) * diag(n)) %*% data)$vectors
-                        # A = A[, c(1:ndim)]
-                        
-                        testobj <- t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * diag(n)) %*% data
-                        
-                        A <- partial_eigen(x=testobj, n = ndim, symmetric = TRUE)$vectors
-                        
-                        
-                        G = data %*% A
-                        Y = pseudoinverse(t(U) %*% U) %*% t(U) %*% G
-                        f = alpha * ssq(data - G %*% t(A)) + (1 - alpha) * 
-                            ssq(data %*% A - U %*% Y)
-                    }
-                    func[run] = f
-                    FF[[run]] = G
-                    AA[[run]] = A
-                    YY[[run]] = Y
-                    UU[[run]] = U
-                    cat("Just finished iteration ", run, "\n")
-                }
-                mi = which.min(func)
-                U = UU[[mi]]
-                cluID = apply(U, 1, which.max)
-                csize = round((table(cluID)/sum(table(cluID))) * 100, digits = 2)
-                aa = sort(csize, decreasing = TRUE)
-                require(plyr)
-                cluID = mapvalues(cluID, from = as.integer(names(aa)), to = as.integer(names(table(cluID))))
-                centroid = YY[[mi]]
-                centroid = centroid[as.integer(names(aa)), ]
-                if (rotation == "varimax") {
-                    require(stats)
-                    AA[[mi]] = varimax(AA[[mi]])$loadings
-                    FF[[mi]] = data %*% AA[[mi]]
-                    centroid = pseudoinverse(t(U) %*% U) %*% t(U) %*% FF[[mi]]
-                    centroid = centroid[as.integer(names(aa)), ]
-                }
-                else if (rotation == "promax") {
-                    AA[[mi]] = promax(AA[[mi]])$loadings[1:m, 1:ndim]
-                    FF[[mi]] = data %*% AA[[mi]]
-                    centroid = pseudoinverse(t(U) %*% U) %*% t(U) %*% FF[[mi]]
-                    centroid = centroid[as.integer(names(aa)), ]
-                }
-                out = list()
-                mi = which.min(func)
-                out$obscoord = FF[[mi]]
-                rownames(out$obscoord) = rownames(data)
-                out$attcoord = data.matrix(AA[[mi]])
-                rownames(out$attcoord) = colnames(data)
-                out$centroid = centroid
-                names(cluID) = rownames(data)
-                out$cluID = cluID
-                out$criterion = func[mi]
-                out$csize = round((table(cluID)/sum(table(cluID))) * 100, 
-                    digits = 1)
-                out$odata = odata
-                out$scale = scale
-                out$center = center
-                out$nstart = nstart
-                class(out) = "cluspca"
-                return(out)
-            }
-            
-            
             ###
             ### Run RKM on the normalized W_tilde matrix
             ###
@@ -559,5 +253,156 @@ clustering <- function(Weights, method, k, t=NULL, sparse=T,
     
     
     
-    
+###
+### Define the RKM function
+###
+
+RKM <- function (data, nclus, ndim, alpha = NULL, method = "RKM", center = TRUE, 
+    scale = TRUE, rotation = "none", nstart = 10, smartStart = NULL, 
+    seed = 1234) {
+
+    require(ggplot2)
+    require(dummies)
+    require(grid)
+    require(corpcor)
+
+    ssq = function(a) {
+        t(as.vector(c(as.matrix(a))))%*%as.vector(c(as.matrix(a)))
+    }
+
+    if (is.null(alpha) == TRUE) {
+        if (method == "RKM") {
+            alpha = 0.5
+        }
+        else if (method == "FKM") {
+            alpha = 0
+        }
+    }
+    odata = data
+    data = scale(data, center = center, scale = scale)
+    # data = data.matrix(data)
+    n = dim(data)[1]
+    m = dim(data)[2]
+    conv = 1e-06
+    func = {
+    }
+    index = {
+    }
+    AA = {
+    }
+    FF = {
+    }
+    YY = {
+    }
+    UU = {
+    }
+
+    require(irlba)
+
+    for (run in c(1:nstart)) {
+        if (is.null(smartStart)) {
+            myseed = seed + run
+            set.seed(myseed)
+            randVec = matrix(ceiling(runif(n) * nclus), n, 1)
+        }
+        else {
+            randVec = smartStart
+        }
+        U = dummy(randVec)
+        P = U %*% pseudoinverse(t(U) %*% U) %*% t(U)
+
+
+        # A = eigen(t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * 
+        #         diag(n)) %*% data)$vectors
+        # A = A[, 1:ndim]
+
+
+        testobj <- t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * diag(n)) %*% data
+
+        A <- partial_eigen(x=testobj, n = ndim, symmetric = TRUE)$vectors
+
+
+        G = data %*% A
+        Y = pseudoinverse(t(U) %*% U) %*% t(U) %*% G
+        f = alpha * ssq(data - G %*% t(A)) + (1 - alpha) * ssq(data %*% 
+                A - U %*% Y)
+        f = as.numeric(f)
+        fold = f + 2 * conv * f
+        iter = 0
+        while (f < fold - conv * f) {
+            fold = f
+            iter = iter + 1
+            outK = try(kmeans(G, centers = Y, nstart = 100), 
+                silent = T)
+            if (is.list(outK) == FALSE) {
+                outK = EmptyKmeans(G, centers = Y)
+            }
+            v = as.factor(outK$cluster)
+            U = diag(nlevels(v))[v, ]
+            P = U %*% pseudoinverse(t(U) %*% U) %*% t(U)
+
+            # A = eigen(t(data) %*% ((1 - alpha) * P - (1 - 2 * 
+            #         alpha) * diag(n)) %*% data)$vectors
+            # A = A[, c(1:ndim)]
+
+            testobj <- t(data) %*% ((1 - alpha) * P - (1 - 2 * alpha) * diag(n)) %*% data
+
+            A <- partial_eigen(x=testobj, n = ndim, symmetric = TRUE)$vectors
+
+
+            G = data %*% A
+            Y = pseudoinverse(t(U) %*% U) %*% t(U) %*% G
+            f = alpha * ssq(data - G %*% t(A)) + (1 - alpha) * 
+                ssq(data %*% A - U %*% Y)
+        }
+        func[run] = f
+        FF[[run]] = G
+        AA[[run]] = A
+        YY[[run]] = Y
+        UU[[run]] = U
+        cat("Just finished iteration ", run, "\n")
+    }
+    mi = which.min(func)
+    U = UU[[mi]]
+    cluID = apply(U, 1, which.max)
+    csize = round((table(cluID)/sum(table(cluID))) * 100, digits = 2)
+    aa = sort(csize, decreasing = TRUE)
+    require(plyr)
+    cluID = mapvalues(cluID, from = as.integer(names(aa)), to = as.integer(names(table(cluID))))
+    centroid = YY[[mi]]
+    centroid = centroid[as.integer(names(aa)), ]
+    if (rotation == "varimax") {
+        require(stats)
+        AA[[mi]] = varimax(AA[[mi]])$loadings
+        FF[[mi]] = data %*% AA[[mi]]
+        centroid = pseudoinverse(t(U) %*% U) %*% t(U) %*% FF[[mi]]
+        centroid = centroid[as.integer(names(aa)), ]
+    }
+    else if (rotation == "promax") {
+        AA[[mi]] = promax(AA[[mi]])$loadings[1:m, 1:ndim]
+        FF[[mi]] = data %*% AA[[mi]]
+        centroid = pseudoinverse(t(U) %*% U) %*% t(U) %*% FF[[mi]]
+        centroid = centroid[as.integer(names(aa)), ]
+    }
+    out = list()
+    mi = which.min(func)
+    out$obscoord = FF[[mi]]
+    rownames(out$obscoord) = rownames(data)
+    out$attcoord = data.matrix(AA[[mi]])
+    rownames(out$attcoord) = colnames(data)
+    out$centroid = centroid
+    names(cluID) = rownames(data)
+    out$cluID = cluID
+    out$criterion = func[mi]
+    out$csize = round((table(cluID)/sum(table(cluID))) * 100, 
+        digits = 1)
+    out$odata = odata
+    out$scale = scale
+    out$center = center
+    out$nstart = nstart
+    class(out) = "cluspca"
+    return(out)
+}
+            
+            
     
